60bff800036c018eec312b2ff78acd86480e73d3
KCsmallz <32394150+KCsmallz@users.noreply.github.com>
2017-10-24 13:29:03 -0400

    asdasd
    asdasd

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
     </head>
     <body>
         <style>
+            div {
+    height: 200px;
+    width: 400px;
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-top: -100px;
+    margin-left: -200px;
+}
         body {
   height: 100%;
 }
