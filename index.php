00ef81ee8992373f4bbdf9f1f9a7e664d8a2ca79
Justin Proulx (aka Sn0wCh1ld) <jawproulx2001@icloud.com>
2017-10-24 12:26:26 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <br>
+        <br>
+        <div class="container" style="text-align:center">
+            
+            <form method="post"  action="?action">
+                <label><b>Username: </b></label>
+                <input type="text" placeholder="Username" name="uname" required><br><br>
+
+                <label><b>Password: </b></label>
+                <input type="password" placeholder="Password" name="pword" required><br><br>
+                <input type="hidden" name="act" value="run">
+                <button type="submit" name="login">Log In</button>
+            </form>
+        
+            <?php
+                // put your code here
+                
+                echo "<br><br><br>";
+            
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "userDB";
+        
+                // Create connection
+                $connection = new mysqli($servername, $username, $password, $dbname);
+        
+                // Check connection
+                if ($connection->connect_error) {
+                    die("Connection failed: " . $connection->connect_error);
+                }
+        
+                // Create database
+                $createDB = "CREATE DATABASE IF NOT EXISTS userDB";
+                $createTable = "CREATE TABLE IF NOT EXISTS userTable"
+                             . "("
+                             . "id INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,"
+                             . "username VARCHAR(30) NOT NULL,"
+                             . "password VARCHAR(256) NOT NULL,"
+                             . "reg_date TIMESTAMP"
+                             . ")";
+        
+                if ($connection->query($createDB) === TRUE)
+                {
+                    echo "Database created successfully, or already exists" . "<br>";
+                } 
+                else 
+                {
+                    echo "Error creating database: " . $connection->error;
+                }
+        
+                // Create table
+        
+                if (mysqli_query($connection, $createTable))
+                {
+                    echo "Table created or already exists";
+                }
+                else
+                {
+                    echo "Error creating table: " . mysqli_error($connection);
+                }
+                
+                //submission
+                if(isset($_GET['action']))
+                {
+                    $un = $_POST['uname'];
+                    $pw = $_POST['pword'];
+                    
+                    $hashedpw = hash('sha256', $pw);
+                    
+                    $sql = "INSERT INTO userTable (username, password)
+                            VALUES ('$un', '$hashedpw')";
+                    
+                    if ($connection->query($sql) === TRUE)
+                    {
+                        echo "<br>" . "DICKHEAD";
+                    }
+                    else
+                    {
+                        echo "<br>" . "ERROR: " . $connection->error;
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    //your code
+                    echo "<br>" . 'LOGGED IN';
+                }
+            ?>
+        </div>
+    </body>
+</html>
