ab71074f7f383f21e62c78ec6f4ca2a36aea488e
Justin Proulx (aka Sn0wCh1ld) <jawproulx2001@icloud.com>
2017-10-27 12:35:39 -0400

    actually calls the function now

--- index.php
+++ index.php
@@ -171,6 +171,8 @@
                     if($nombreRangées > 0)
                     {
                         echo "<br>CONNECTION FONCTIONNE";
+                        
+                        affichierInformation($connection, $nom);
                     }
                     else
                     {
